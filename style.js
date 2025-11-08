21006a6f385ac29d254f890847d1d3c321d1a14f
IhenyenSolomon <ihenyensolomon@gmail.com>
2025-11-08 07:46:49 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,16 @@
+// const header = document.querySelector('#hero');
+// const images = [ "img/bcg1.jpg", "img/bcg2.jpg", "img/bcg3.jpg", "img/bcg4.jpg"]
+// console.log(images);
+// let index = 0;
+
+// function changeBackground(){
+//     header.classList.add('fade');
+//     setTimeout(() => {
+//         header.classList.remove('fade');
+//         index = (index + 1) % images.length;
+//         console.log("Now showing", images[index])
+//         header.style.backgroundImage = images[index];
+//      }, 1000)
+// }
+
+// setInterval(changeBackground,5000)
